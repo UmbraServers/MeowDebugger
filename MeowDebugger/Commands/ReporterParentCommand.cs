c5f4089ee8917faac8db6641c128d05bc6db7bc8
Unbistrackted <112902220+Unbistrackted@users.noreply.github.com>
2026-03-15 10:21:11 -0300

    feat: use parent command instead of a single command

---
+++ MeowDebugger/Commands/ReporterParentCommand.cs
@@ -0,0 +1,58 @@
+﻿using CommandSystem;
+using LabApi.Features.Wrappers;
+using MeowDebugger.API.Features;
+using MeowDebugger.Commands.SubCommand;
+using System;
+
+namespace MeowDebugger.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    [CommandHandler(typeof(GameConsoleCommandHandler))]
+    public class ReporterParentCommand : ParentCommand
+    {
+        public ReporterParentCommand() => this.LoadGeneratedCommands();
+        public override string Command => "reporter";
+
+        public override string[] Aliases => [];
+
+        public override string Description => "Reports the slowest methods";
+
+        public override void LoadGeneratedCommands()
+        {
+            // I wanted to use attributes for this cause it sounds fun but I think I will be just overcomplicating stuff 
+#if EXILED_RELEASE
+            this.RegisterCommand(new DisableCommand());
+            this.RegisterCommand(new EnableCommand());
+            this.RegisterCommand(new FilterCommand());
+            this.RegisterCommand(new FlameCommand());
+            this.RegisterCommand(new HelpCommand());
+            this.RegisterCommand(new TopCommand());
+            this.RegisterCommand(new TpsCommand());
+#endif
+        }
+
+        protected override bool ExecuteParent(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (arguments.Count == 0)
+            {
+                response = WithTps(MethodMetrics.ReportAndReset());
+                return true;
+            }
+
+            response = $"Unknown subcommand. Use 'reporter help' for usage.";
+            return false;
+        }
+
+        internal static string WithTps(string? metrics)
+        {
+            double tps = Server.Tps >= Server.MaxTps ? Server.MaxTps : Server.Tps;
+
+            if (tps < 0)
+            {
+                tps = 0;
+            }
+                
+            return $"TPS: {tps:0.##}\n{metrics}";
+        }
+    }
+}
---
+++ MeowDebugger/Commands/SubCommand/HelpCommand.cs
@@ -0,0 +1,31 @@
+﻿using CommandSystem;
+using System;
+
+namespace MeowDebugger.Commands.SubCommand
+{
+#if !EXILED_RELEASE
+    [CommandHandler(typeof(ReporterParentCommand))]
+#endif
+    public class HelpCommand : ICommand
+    {
+        public string Command => "help";
+
+        public string[] Aliases => [];
+
+        public string Description => "Usage of the reporter command.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response =
+                "\nreporter              - Report & reset all metrics\n" +
+                "reporter enable       - Enable method metrics\n" +
+                "reporter disable      - Disable method metrics\n" +
+                "reporter top <N>      - Report top N slowest methods\n" +
+                "reporter flame <time> - Exports speedscope graph (optional: profiles in a certain amount of time)\n" +
+                "reporter filter <...> - Report specific methods by name\n" +
+                "reporter help         - Show this help\n" + 
+                "reporter tps <time>   - Measures TPS in a certain amount of time";
+            return true;
+        }
+    }
+}
